041143d5a9d7d2c7cdc531e4dd6da69c4790efdb
Santiago <spedrazamoya@gmail.com>
2025-10-22 18:16:25 +0200

    Update ModItems.java

--- src/main/java/com/example/fantastic_caves/item/ModItems.java
+++ src/main/java/com/example/fantastic_caves/item/ModItems.java
@@ -8,6 +8,8 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+
+
 public class ModItems {
 
     // =========================
