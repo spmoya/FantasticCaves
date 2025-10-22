9e533882dd374bdfafaf1de8247629cd7ccece11
Santiago <spedrazamoya@gmail.com>
2025-10-22 15:37:05 +0200

    Initial commit

---
+++ src/client/java/com/example/fantastic_caves/FantasticcavesClient.java
@@ -0,0 +1,10 @@
+package com.example.fantastic_caves;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FantasticcavesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/example/fantastic_caves/item/ModItems.java
@@ -0,0 +1,10 @@
+package com.example.fantastic_caves.item;
+
+import com.example.fantastic_caves.Fantasticcaves;
+
+public class ModItems {
+
+    public static void registerModItems() {
+        Fantasticcaves.LOGGER.info("Registering Mod Items for " + Fantasticcaves.MOD_ID);
+    }
+}
